b4ff7ef12b3fbab6f61f0f38408334e5810915b5
root <root@localhost.localdomain>
2014-10-09 13:12:31 +0000

    plus besoin de proxy

--- src/Axa/Bundle/WhsysBundle/Resources/Portail.php
+++ src/Axa/Bundle/WhsysBundle/Resources/Portail.php
@@ -5,7 +5,7 @@ use Guzzle\Http\Client;
  *
 */
 class Portail{
-	var $proxy="http://10.225.23.242:8080/";
+//	var $proxy="http://10.225.23.242:8080/";
 	var $base_url="http://portal.axa-cloud.com/";
 	var $cookie="";
 	var timeout = 60;
@@ -31,7 +31,7 @@ class Portail{
     public function post($suffix,$jsondata){
 		return $response = $this->client->post($this->$base_url.$suffix	, [
 			'headers' => ['Content-Type' => 'application/json', "Content-Length" => strlen($jsondata)],
-			'proxy'	  => $this->proxy,
+//			'proxy'	  => $this->proxy,
 			'timeout' => $this->timeout,
 			'cookies' => ['JSESSIONID' => $this->cookie.'; path=/; domain=portal.axa-cloud.com; HttpOnly']
 		]);
@@ -45,7 +45,7 @@ class Portail{
     public function put($suffix,$jsondata){
 		return $response = $this->client->put($this->$base_url.$suffix	, [
 			'headers' => ['Content-Type' => 'application/json', "Content-Length" => strlen($jsondata)],
-			'proxy'	  => $this->proxy,
+//			'proxy'	  => $this->proxy,
 			'timeout' => $this->timeout,
             'cookies' => ['JSESSIONID' => $this->cookie.'; path=/; domain=portal.axa-cloud.com; HttpOnly']
 		]);
@@ -57,7 +57,7 @@ class Portail{
      */
     public function get($suffix){
 		return $response = $this->client->get($this->$base_url.$suffix	, [
-			'proxy'	  => $this->proxy,
+//			'proxy'	  => $this->proxy,
 			'timeout' => $this->timeout,
 			'cookies' => ['JSESSIONID' => $this->cookie.'; path=/; domain=portal.axa-cloud.com; HttpOnly']
 		]);
